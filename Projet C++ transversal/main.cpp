e4bb51449af26429e30d2e7d6119fa45ed6f877e
arcreaneDev <dominique@arcreane.com>
2021-03-03 14:41:36 +0100

    Code clean up Use of variables instead of hard coded value

--- Projet C++ transversal/main.cpp
+++ Projet C++ transversal/main.cpp
@@ -18,8 +18,11 @@ int main(int argc, char** argv)
         cout << "Could not open or find the image" << std::endl;
         return -1;
     }
-    namedWindow("Display window", WINDOW_AUTOSIZE); // Create a window for display.
-    imshow("Display window", image); // Show our image inside it.
+
+    std::string windowName = "Display window";
+        
+    namedWindow(windowName, WINDOW_AUTOSIZE); // Create a window for display.
+    imshow(windowName, image); // Show our image inside it.
     waitKey(0); // Wait for a keystroke in the window
     return 0;
 }
