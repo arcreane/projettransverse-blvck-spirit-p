fc2dc52ecac3aa1d9e9dae53c57b373d44be565b
thibault chu <thibaultchu@hotmail.fr>
2021-03-17 07:58:49 +0100

    Utilisation de la webcam

--- Projet C++ transversal/main.cpp
+++ Projet C++ transversal/main.cpp
@@ -4,25 +4,19 @@
 #include <iostream>
 using namespace cv;
 using namespace std;
-int main(int argc, char** argv)
-{
-    if (argc != 2)
-    {
-        cout << " Usage: " << argv[0] << " ImageToLoadAndDisplay" << endl;
-        return -1;
-    }
-    Mat image;
-    image = imread(argv[1], IMREAD_COLOR); // Read the file
-    if (image.empty()) // Check for invalid input
-    {
-        cout << "Could not open or find the image" << std::endl;
-        return -1;
-    }
 
-    std::string windowName = "Display window";
-        
-    namedWindow(windowName, WINDOW_AUTOSIZE); // Create a window for display.
-    imshow(windowName, image); // Show our image inside it.
-    waitKey(0); // Wait for a keystroke in the window
-    return 0;
+void main() {
+
+	VideoCapture cap(0);
+	Mat img;
+
+	while (true) {
+		
+		cap.read(img);
+
+		imshow("Image", img);
+		waitKey(1);
+	}
+
+
 }
